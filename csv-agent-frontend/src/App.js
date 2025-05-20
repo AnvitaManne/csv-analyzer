239f5cbd2046706db47abc529ac7ecc1bfe937d9
Anvita Manne <anvita.manne@gmail.com>
2025-05-20 23:54:10 +0530

    Integrated csv-agent-frontend as a regular directory and cleaned up gitignore

---
+++ csv-agent-frontend/src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React from "react";
+import UploadForm from "./UploadForm";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> </h1> 
+      </header>
+      <main> 
+        <UploadForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;
